e5ec06e2de625583e207ee62aa01eb7227fb430a
hoja <sultanhoja228@gmail.com>
2023-06-08 20:13:16 +0600

    review

--- review/models.py
+++ review/models.py
@@ -2,5 +2,21 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from orders.models import Order
 
+User = get_user_model()
 
+class Comment(models.Model):
+    ordrer = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments', verbose_name='Автор')
+    body = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.body
+    
+class RatingUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings', verbose_name='Автор')
+    rating = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.rating
--- review/views.py
+++ review/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from .serializers import CommentSerializer, RatingSerializer
+from .models import Comment, RatingUser
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+
+class PermissionMixin:
+    def get_permissions(self):
+        if self.action == 'create':
+            permissions = [IsAuthorOrReadOnly]
+        elif self.action in ['update', 'partial_update', 'destroy']:
+            permissions = [IsAuthorOrReadOnly]
+        else:
+            permissions = [AllowAny]
+        return [permission() for permission in permissions]
+
+
+
+class CommentViewSet(PermissionMixin ,ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [PermissionMixin]
+
+    
+class RatingViewSet(PermissionMixin, ModelViewSet):
+    queryset = RatingUser.objects.all()
+    serializer_class = RatingSerializer
+    permission_classes = [PermissionMixin]
